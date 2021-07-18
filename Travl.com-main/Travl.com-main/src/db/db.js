50227dc90a7f6534382e49d109dc317380bb224e
chetan1NT18IS044 <82074796+chetan1NT18IS044@users.noreply.github.com>
2021-07-19 00:15:46 +0530

    wt_proj

---
+++ Travl.com-main/Travl.com-main/src/db/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/chetan2",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).then(() => {
+    console.log(`connection successful`);
+}).catch((e)=>{
+    console.log(`no connection`);
+});
